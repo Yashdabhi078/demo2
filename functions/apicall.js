1a75596053f6a48d670ff6c46803357a5f65650c
Yash <yash.dabhi@marutitech.com>
2021-05-04 18:24:37 +0530

    commit

--- functions/apicall.js
+++ functions/apicall.js
@@ -1,25 +1,25 @@
-const fetch = require('node-fetch')
+// const fetch = require('node-fetch')
 
-const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts'
+// const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts'
 
 exports.handler = async (event, context) => {
-  let response
-  try {
-    response = await fetch(API_ENDPOINT)
-    // handle response
-  } catch (err) {
-    return {
-      statusCode: err.statusCode || 500,
-      body: JSON.stringify({
-        error: err.message
-      })
-    }
-  }
+  // let response
+  // try {
+  //   response = await fetch(API_ENDPOINT)
+  //   // handle response
+  // } catch (err) {
+  //   return {
+  //     statusCode: err.statusCode || 500,
+  //     body: JSON.stringify({
+  //       error: err.message
+  //     })
+  //   }
+  // }
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      data: response
+      data: "response"
     })
   }
 }
