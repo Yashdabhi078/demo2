fa05dc999022b0176c72c9628b8d7e1a78798829
Yash <yash.dabhi@marutitech.com>
2021-05-06 12:57:43 +0530

    Apicall working

--- serverless/functions_y/apicall/apicall.js
+++ serverless/functions_y/apicall/apicall.js
@@ -1,6 +1,30 @@
-exports.handler = async function(event, context) {
+const fetch = require('node-fetch')
+
+
+exports.handler = async (event, context) => {
+  const body = JSON.parse(event.body)
+  
+  let response
+  try {
+    response = await fetch(body.url,{
+      method: body.type,
+      headers: body.headers,
+      body: JSON.stringify(body.body)
+    })
+    // handle response
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message
+      })
+    }
+  }
+  const y = await response.json()
   return {
-      statusCode: 200,
-      body: JSON.stringify({message: "Hello World"})
-  };
+    statusCode: 200,
+    body: JSON.stringify({
+      data: y
+    })
+  }
 }
